9dba38f471716535f100edf106729316548fb6f5
WanderAndrade <wanderdeveloper01@gmail.com>
2020-07-01 16:36:43 -0300

    Alteração 2

---
+++ RestWithASPNETUdemy 01 - Calculator/RestWithASPNETUdemy/Controllers/CalculatorController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    [Route("api/[controller]")]
+    public class CalculatorController : Controller
+    {
+
+        // GET api/values/5/5
+        [HttpGet("{firstNumber}/{secondNumber}")]
+        public IActionResult Sum(string firstNumber, string secondNumber)
+        {
+                if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        private decimal ConvertToDecimal(string firstNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool IsNumeric(string firstNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
